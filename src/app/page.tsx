4a24d5a145fac5464351e491fea02787e258e2fa
gllamat <62991426+gllamat@users.noreply.github.com>
2026-04-05 22:54:13 +0100

    sidebar bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
 "use client";
 
-import { Navbar } from "@/components/Navbar";
-import { Sidebar } from "@/components/Sidebar";
 import DemoJourney from "@/components/DemoJourney";
 
 export default function Home() {
-  return (
-    <div className="flex bg-navy min-h-screen text-white font-dm-sans selection:bg-accent selection:text-navy">
-      <Sidebar />
-      <div className="flex-1 flex flex-col">
-        <Navbar />
-        <main className="flex-1">
-          <DemoJourney />
-        </main>
-      </div>
-    </div>
-  );
+  return <DemoJourney />;
 }
